548002f034cd2bec4fc9bee0d0d43b7a6667e55c
Harryharries <a1021604754@gmail.com>
2020-06-02 18:06:46 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# -Harry_GuApi_Blog
+
+A personal blog which host by AWS 
+
+DB : Mongo DB
+Server: Node.js + AWS
---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+
+
+//since  80 is default
+app.listen(80)
+console.log("open service, please go to localhost in browser")
